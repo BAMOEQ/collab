2e36d118a9f6f08842f1303e965f3f6b73715098
Brandon Morales <bam@10-18-165-53.dynapool.wireless.nyu.edu>
2024-11-06 17:55:32 -0500

    First

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-        <blockquote cite= "">
-        </p>
+        <img src={logo} className="App-logo" alt="logo" ></img>
+          <body>
+
+  <header id="Brandon"> fouvuhdfhwedhewljclewjncljerwnc</header>
+Brandon
+  </body>
         <a
           className="App-link"
           href="https://reactjs.org"
